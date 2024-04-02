253d66aca1559bb50aca506d5625f0c77287afc0
y9048a <johannes.kanefendt@krzn.de>
2024-04-02 20:52:06 +0200

    Use PID for heating control, refactored loop

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,22 @@
 #include <MAX6675.h>
 #include <movingAvg.h>
 #include <SPIFFS.h>
+#include <PID_v1.h>
+
+#define   PID_P                   80
+#define   PID_I                   75
+#define   PID_D                   0
+
+#define   PIN_MAX6675_SELECT      33
+#define   PIN_MAX6675_MISO        25  
+#define   PIN_MAX6675_CLOCK       32
+
+#define   PIN_RELAY_HEATING       18
+
+#define   XDB401_MAX_BAR          20
+
+#define   CYCLE_LENGTH            40
+#define   MAX6675_DUTY_CYCLES     6
 
 int ReadXdb401PressureValue(int *result);
 
@@ -12,79 +28,93 @@ TFT_eSPI tft = TFT_eSPI();
 
 MAX6675 thermoCouple(33, 35, 32);
 
-movingAvg avgTemp(10); 
-movingAvg avgPressure(16); 
+movingAvg temperateAvg(10), pressureAvg(10);
+
+double temperatureSet, temperatureIs, pidOut;
+
+PID temperaturePid(&temperatureIs, &pidOut, &temperatureSet, PID_P, PID_I, PID_D, DIRECT);
 
 void setup()
 {
   Serial.begin(9600);
 
   Wire.begin();
+  SPIFFS.begin();
+  thermoCouple.begin();
 
-  delay(1000);
+  pinMode(PIN_RELAY_HEATING, OUTPUT);
 
   tft.init();
   tft.setRotation(0);
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
-
-  SPIFFS.begin();
-
   tft.loadFont("NotoSansBold36");
 
-  thermoCouple.begin();
-  avgTemp.begin(); 
-  avgPressure.begin();
+  temperateAvg.begin(); 
+  pressureAvg.begin();
+
+  temperatureSet = 30;
+
+  temperaturePid.SetOutputLimits(0, MAX6675_DUTY_CYCLES * CYCLE_LENGTH);
+  temperaturePid.SetMode(AUTOMATIC);
 }
 
-int c = 0;
+unsigned long c = 0;
+unsigned long heatingDueTime;
 
 void loop()
 {
   c++;
-  
+
+  unsigned long windowStart = millis();
+
+  if (windowStart > heatingDueTime) 
+  {
+      digitalWrite(PIN_RELAY_HEATING, LOW);
+  }
+
   uint16_t x = tft.width() / 2;
   uint16_t y = tft.height() / 2;
 
-  int pressureValue;
-  if (ReadXdb401PressureValue(&pressureValue) == 0)
+  if (c % MAX6675_DUTY_CYCLES == 0)
   {
-      // Pressure samplerate: 100Hz
-
-      // Extrapolate 24bit value to 16bit 
-      short pressure = (short)(pressureValue / 256);
-      avgPressure.reading(pressure);
-  }
-
-  if (c % 4 == 0)
-  {
-    // 25fps for pressure gauge
-
-      float avgPressureBar = avgPressure.getAvg() / 32768.0 * 20;
-
-      tft.setTextDatum(MR_DATUM);
-      tft.setTextPadding(tft.textWidth("00.0"));
-      tft.drawFloat(avgPressureBar, 1, x, y);      
-  }
-
-  if (c % 22 == 0)
-  {
-    // Read temperature every 220ms (maximum sample rate for max6675)
-
     int status = thermoCouple.read();
 
-    int16_t tempValue = (thermoCouple.getRawData() >> 3) & 0x1FFF;
-    avgTemp.reading(tempValue);
-  }
+    int16_t temperature = (thermoCouple.getRawData() >> 3) & 0x1FFF;
+    temperatureIs = temperature / 4.0;
+    temperaturePid.Compute();
 
-  if (c % 50 == 0)
-  {
-    // 1fps for temperature gauge
+    if (pidOut > 0) 
+    {
+      digitalWrite(PIN_RELAY_HEATING, HIGH);
+      heatingDueTime = windowStart + (int) pidOut;
+    }
 
+    temperateAvg.reading(temperature);
+
+    if (c % (MAX6675_DUTY_CYCLES * 4) == 0)
+    {
       tft.setTextDatum(MR_DATUM);
-      tft.setTextPadding(tft.textWidth("00.0"));
-      tft.drawFloat(avgTemp.getAvg() / 4.0, 1, x, y + 40);
+      tft.setTextPadding(tft.textWidth("000.0"));
+      tft.drawFloat(temperateAvg.getAvg() / 4.0, 1, x, y + 40);
+    }
   }
-  
-  delay(10);
+
+  int pressureSample;
+  if (ReadXdb401PressureValue(&pressureSample) == 0)
+  {
+      pressureAvg.reading((short)(pressureSample / 256));
+
+      float pressure = pressureAvg.getAvg() / float(SHRT_MAX) * XDB401_MAX_BAR;
+      tft.setTextDatum(MR_DATUM);
+      tft.setTextPadding(tft.textWidth("000.0"));
+      tft.drawFloat(pressure, 1, x, y);      
+  }
+
+  unsigned long windowEnd = millis();
+  unsigned int elapsed = windowEnd - windowStart;
+  if (elapsed < CYCLE_LENGTH)
+  {
+    delay(CYCLE_LENGTH - elapsed);
+  }
 }
